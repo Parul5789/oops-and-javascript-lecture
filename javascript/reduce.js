db9068deb51cd09e4cabdba87611598f20adde8a
Parul Singh <sejalpsingh0987@gmail.com>
2025-07-24 00:15:35 +0530

    cssfile changes

--- javascript/reduce.js
+++ javascript/reduce.js
@@ -2,14 +2,7 @@ let arr=[19,4,76,20,9];
 // let output=arr.reduce((res , val)=>{
 //     return res+val;
 // })
-// let output=arr.reduce((res , val)=>{
-//     return res>val? res:val;
-// })
-// console.log(output);
-let output=arr.filter((val)=>{
-    if(val>=9){
-        return val;
-
-    }
-});
+let output=arr.reduce((res , val)=>{
+    return res>val? res:val;
+})
 console.log(output);
